72c8d24943feca0014f4aa75a1797c3a8b920dd5
David Balivo <david.balivo@gmail.com>
2024-08-10 22:02:10 +0200

    Variables set to private

--- binance_client.py
+++ binance_client.py
@@ -1,10 +1,10 @@
 from binance.client import Client
 from config import get_api_key, get_secret_key
 
-api_key = get_api_key()
-secret_key = get_secret_key()
+_api_key = get_api_key()
+_secret_key = get_secret_key()
 
-client = Client(api_key, secret_key)
+_client = Client(_api_key, _secret_key)
 
 def get_binance_client():
-    return client
+    return _client
