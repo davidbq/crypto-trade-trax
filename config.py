9fad54a3322e7f7e3586b85d6898a332d213b2cd
David Balivo <david.balivo@gmail.com>
2024-08-13 16:11:40 +0200

    Minor changes

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from os import getenv
 
 load_dotenv()
 
-_api_key = getenv('API_KEY')
-_secret_key = getenv('SECRET_KEY')
+api_key = getenv('API_KEY')
+secret_key = getenv('SECRET_KEY')
 
 def get_api_key():
-    return _api_key
+    return api_key
 
 def get_secret_key():
-    return _secret_key
+    return secret_key
