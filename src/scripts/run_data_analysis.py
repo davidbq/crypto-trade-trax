44943d5868c45a8160a36dba33fbc39e07d0a765
David Balivo <david.balivo@gmail.com>
2024-08-16 12:09:23 +0200

    Added function explanation to script run data analysis

--- src/scripts/run_data_analysis.py
+++ src/scripts/run_data_analysis.py
@@ -7,6 +7,12 @@ from ..globals.constants import DATAFRAME_COLUMN_NAMES, CSV_PATHS
 from ..config.logging import info
 
 def build_weekly_metrics_df(df: DataFrame) -> DataFrame:
+    '''
+    Processes a DataFrame containing daily cryptocurrency price data to calculate weekly percentage changes,
+    starting from the first complete week (Monday to Sunday) in the dataset. The resulting DataFrame provides
+    a pivot table with the percentage change for each day of the week, indexed by week number.
+    '''
+
     DAY_OF_WEEK = DATAFRAME_COLUMN_NAMES['DAY_OF_WEEK']
     WEEK_NUMBER = DATAFRAME_COLUMN_NAMES['WEEK_NUMBER']
     OPEN_PRICE = DATAFRAME_COLUMN_NAMES['OPEN_PRICE']
@@ -30,15 +36,30 @@ def build_weekly_metrics_df(df: DataFrame) -> DataFrame:
     return df[DAY_OF_WEEK_NAMES]
 
 def build_cosine_sim_df(df: DataFrame) -> DataFrame:
+    '''
+    Calculates the cosine similarity matrix for a given DataFrame, where each row represents a different
+    week, and each column represents the percentage change on a specific day of the week.
+    '''
+
     cosine_sim_matrix = cosine_similarity(df.fillna(0))
     return DataFrame(cosine_sim_matrix, index=df.index, columns=df.index)
 
-def extract_top_n_similarities(df_cosine_sim: DataFrame, top_n: int) -> Series:
+def extract_top_n_similarities(df: DataFrame, top_n: int) -> Series:
+    '''
+    Extracts the top N cosine similarities from the cosine similarity matrix, ignoring self-comparisons
+    and duplicate comparisons.
+    '''
+
     # Ignore diagonal (self-comparisons) and lower triangle duplicates
-    df_cosine_sim.values[tril_indices_from(df_cosine_sim)] = -1
-    return df_cosine_sim.unstack().sort_values(ascending=False).head(top_n)
+    df.values[tril_indices_from(df)] = -1
+    return df.unstack().sort_values(ascending=False).head(top_n)
 
 def run_analysis():
+    '''
+    Orchestrates the data analysis process by loading datasets, calculating weekly metrics, computing
+    cosine similarities, and plotting the results.
+    '''
+
     info("Starting data analysis process.")
     datasets = {
         'BTC_HISTORICAL': CSV_PATHS['BTC_HISTORICAL'],
