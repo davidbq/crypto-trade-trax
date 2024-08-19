00d1106493cd615058ec4cee9000dcb99837df39
David Balivo <david.balivo@gmail.com>
2024-08-19 22:57:44 +0200

    Refactored run_data_analysis to work with the new data structure

--- src/scripts/run_data_analysis.py
+++ src/scripts/run_data_analysis.py
@@ -1,38 +1,9 @@
 from pandas import DataFrame, Series
 from sklearn.metrics.pairwise import cosine_similarity
 from numpy import tril_indices_from
-from ..data.csv_cleaner import load_csv_data
+from ..data.loading import load_csv_data
 from ..utils.plot import plot_dataframe, plot_week_similarities
-from ..globals.constants import DATAFRAME_COLUMN_NAMES, CSV_PATHS
-
-def build_weekly_metrics_df(df: DataFrame) -> DataFrame:
-    '''
-    Processes a DataFrame containing daily cryptocurrency price data to calculate weekly percentage changes,
-    starting from the first complete week (Monday to Sunday) in the dataset. The resulting DataFrame provides
-    a pivot table with the percentage change for each day of the week, indexed by week number.
-    '''
-
-    DAY_OF_WEEK = DATAFRAME_COLUMN_NAMES['DAY_OF_WEEK']
-    WEEK_NUMBER = DATAFRAME_COLUMN_NAMES['WEEK_NUMBER']
-    OPEN_PRICE = DATAFRAME_COLUMN_NAMES['OPEN_PRICE']
-    CLOSE_PRICE = DATAFRAME_COLUMN_NAMES['CLOSE_PRICE']
-    PERCENT_CHANGE = DATAFRAME_COLUMN_NAMES['PERCENT_CHANGE']
-    DAY_OF_WEEK_NAMES = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-    # Find the first Monday in the dataset
-    first_monday_idx = df.index[df.index.day_name() == 'Monday'][0]
-
-    # Filter out the data before this first Monday
-    df = df[df.index >= first_monday_idx]
-
-    df[DAY_OF_WEEK] = df.index.day_name()
-    # Week number relative to the first week in the dataset
-    df[WEEK_NUMBER] = (df.index - df.index.min()).days // 7 + 1
-    df[PERCENT_CHANGE] = ((df[CLOSE_PRICE] - df[OPEN_PRICE]) / df[OPEN_PRICE]) * 100
-
-    df = df.pivot_table(index=WEEK_NUMBER, columns=DAY_OF_WEEK, values=PERCENT_CHANGE)
-
-    return df[DAY_OF_WEEK_NAMES]
+from ..globals.constants import CSV_PATHS
 
 def build_cosine_sim_df(df: DataFrame) -> DataFrame:
     '''
@@ -65,17 +36,14 @@ def run_analysis():
     }
 
     for key in datasets.keys():
-        df_main_data = load_csv_data(CSV_PATHS['CRYPTO'][key])
-        plot_dataframe(df_main_data, f'DataFrame {key}')
+        df = load_csv_data(CSV_PATHS['CRYPTO'][key])
+        plot_dataframe(df, f'DataFrame {key}')
 
-        df_weekly_data = build_weekly_metrics_df(df_main_data)
-        plot_dataframe(df_weekly_data, f'DataFrame Weekly Data {key}')
-
-        df_cosine_sim = build_cosine_sim_df(df_weekly_data)
+        df_cosine_sim = build_cosine_sim_df(df)
         plot_dataframe(df_cosine_sim, f'DataFrame Cosine Similarities {key}')
 
         top_5_similarities = extract_top_n_similarities(df_cosine_sim, 5)
-        plot_week_similarities(df_weekly_data, top_5_similarities)
+        plot_week_similarities(df, top_5_similarities)
 
 
 if __name__ == "__main__":
