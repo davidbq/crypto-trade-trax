58b809bb16f41995ddf6142a271b6a224b462f07
David Balivo <david.balivo@gmail.com>
2024-08-19 22:58:45 +0200

    Removed unused code from date utils

--- src/utils/date.py
+++ src/utils/date.py
@@ -1,12 +1,3 @@
 from datetime import datetime, timedelta
-import calendar
 
 get_yesterday_date = lambda: (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-
-def six_months_ago_monday():
-    today = datetime.today()
-    six_months_ago = today - timedelta(days=6*30)
-    first_of_month = six_months_ago.replace(day=1)
-    days_until_monday = (calendar.MONDAY - first_of_month.weekday() + 7) % 7
-    next_monday = first_of_month + timedelta(days=days_until_monday)
-    return next_monday.strftime('%Y-%m-%d')
