f607262cbd98b50c7b084287985cd10b29fec65a
David Balivo <david.balivo@gmail.com>
2024-08-27 15:34:25 +0200

    Minor improvements for displaying best params

--- src/scripts/display_model_best_params.py
+++ src/scripts/display_model_best_params.py
@@ -4,15 +4,14 @@ from ..config.logging import info
 from ..globals.constants import CSV_PATHS, BEST_PARAMS_DF_COL_NAMES
 
 CSV_PATH = CSV_PATHS['OPT_HP']
+MODEL_PATH = BEST_PARAMS_DF_COL_NAMES['MODEL_PATH']
 
 def load_best_params() -> pd.DataFrame:
     return pd.read_csv(CSV_PATH, index_col='Timestamp', parse_dates=True)
 
 def display_best_params() -> None:
-    MODEL_PATH = BEST_PARAMS_DF_COL_NAMES['MODEL_PATH']
     df = load_best_params()
-    df = df.sort_values(by=[MODEL_PATH, 'MAE test'])
-    if not df.empty:
+    if df is not None and not df.empty:
         plot_dataframe(df, f'Best Parameters for {CSV_PATH}')
     else:
         info(f'No data to display for {CSV_PATH}.')
